8a910bfc65ccec15c72a0a82b3373c80a45e1c1b
Suresh N <snarayanaswamy+pfpt@proofpoint.com>
2025-03-14 01:59:22 +0530

    Saga Pattern

--- Saga Pattern/inventory-service/src/main/java/com/techweb/inventoryservice/listener/InventoryService.java
+++ Saga Pattern/inventory-service/src/main/java/com/techweb/inventoryservice/listener/InventoryService.java
@@ -22,6 +22,6 @@ public class InventoryService {
 
     private boolean checkStockAvailability() {
 
-        return true;
+        return false;
     }
 }
--- Saga Pattern/order-service/src/main/java/com/techweb/orderservice/entity/Order.java
+++ Saga Pattern/order-service/src/main/java/com/techweb/orderservice/entity/Order.java
@@ -1,13 +1,11 @@
 package com.techweb.orderservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
