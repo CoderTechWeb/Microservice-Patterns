721e86c8fb2a8f353e969d851af98dccb5466cea
Suresh N <57557491+CoderTechWeb@users.noreply.github.com>
2025-03-26 17:58:02 +0530

    Postfilter fixed

--- Microservice Security/order-service/src/main/java/com/techweb/orderservice/controller/OrderController.java
+++ Microservice Security/order-service/src/main/java/com/techweb/orderservice/controller/OrderController.java
@@ -1,11 +1,25 @@
 package com.techweb.orderservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')") // ✅ Only ADMIN can access
+    public ResponseEntity<String> getAllOrders() {
+        return ResponseEntity.ok("All Orders (Admin Access)");
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('USER')") // ✅ Only USER can access
+    public ResponseEntity<String> getUserOrders() {
+        return ResponseEntity.ok("User Orders");
+    }
+
 
     @GetMapping("/test")
     public ResponseEntity<String> testOrderService() {
